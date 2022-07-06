61d04362e333eb83d0894b392f666d534042c65f
Anju <anjuk2020@gmail.com>
2022-07-06 09:09:46 +0530

    added the sqlite settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -94,12 +94,16 @@ AUTH_USER_MODEL = 'accounts.Account'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerceproject',
-        'USER':'postgres',
-        'PASSWORD':'a2s1@2022',
-        'HOST':'localhost',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'ecommerceproject',
+    #     'USER':'postgres',
+    #     'PASSWORD':'a2s1@2022',
+    #     'HOST':'localhost',
+    # }
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
