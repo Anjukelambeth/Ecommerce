8c109858e70d456ce25fac8a0fa6fe7d44c18675
Anju <anjuk2020@gmail.com>
2022-07-08 15:16:17 +0530

    checking the sqlite

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -101,19 +101,19 @@ DATABASES = {
     #     'USER':'postgres',
     #     'PASSWORD':'a2s1@2022',
     #     'HOST':'localhost',
-    # }
-    #     'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+    #}
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'myproject',
+    #     'USER': 'myprojectuser',
+    #     'PASSWORD': 'password',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 
 
